190e49642516938d83ec3c511450f0fd77c177cc
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-06-16 16:08:05 +0000

    updated the controller by creating an endpoint that sends mail

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Mail } from './dto/mail.interface';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  async sendEmail(@Body() data: Mail) {
+    await this.appService.sendWelcomeEmail(data);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
+import { InjectQueue } from '@nestjs/bull';
 import { Injectable } from '@nestjs/common';
+import { Queue } from 'bull';
+import { Mail } from './dto/mail.interface';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@InjectQueue('mail') private readonly emailQueue: Queue) {}
+
+  async sendWelcomeEmail(data: Mail) {
+    const job = await this.emailQueue.add('welcome', data);
+
+    return { jobId: job.id };
+  }
+
+  async sendResetPasswordEmail(data: Mail) {
+    const job = await this.emailQueue.add('reset-password', data);
+
+    return { jobId: job.id };
   }
 }

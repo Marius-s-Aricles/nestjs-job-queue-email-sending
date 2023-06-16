7d342a7a05a7ec1273f77ea9e42493bc5d3e5693
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-06-16 16:06:09 +0000

    set up packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,37 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    BullModule.registerQueue({
+      name: 'mail',
+    }),
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.example.com',
+        port: 587,
+        auth: {
+          user: 'username',
+          pass: 'password',
+        },
+      },
+      template: {
+        dir: join(__dirname, 'templates'),
+        adapter: new HandlebarsAdapter(),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
